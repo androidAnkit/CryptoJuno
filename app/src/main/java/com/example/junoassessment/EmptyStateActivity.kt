5025e97ac3ece877989937a0d01e72b4936541e2
Ankit <email2ankit1@gmail.com>
2022-11-12 15:03:07 +0530

    Initial UI design

---
+++ app/src/main/java/com/example/junoassessment/EmptyStateActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cryptobyjuno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.junoassessment.R
+
+class EmptyStateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empty_state)
+    }
+}
